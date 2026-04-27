ec23e981a6132606ff1a23e9fb8a11402ab43cce
nagendra-programmer <nagendrakatta2003@gmail.com>
2026-04-27 08:19:12 +0000

    added new features

--- src/components/MoviesList/index.js
+++ src/components/MoviesList/index.js
@@ -8,14 +8,20 @@ const MoviesList = ({type}) => {
   const [movies, setMovies] = useState([])
   const [page, setPage] = useState(1)
 
+  // Reset page when switching tabs
+  useEffect(() => {
+    setPage(1)
+  }, [type])
+
   useEffect(() => {
     const getMovies = async () => {
       const res = await fetch(
-        `https://api.themoviedb.org/3/movie/${type}?api_key=${API_KEY}&page=${page}`,
+        `https://api.themoviedb.org/3/movie/${type}?api_key=${API_KEY}&language=en-US&page=${page}`,
       )
       const data = await res.json()
       setMovies(data.results || [])
     }
+
     getMovies()
   }, [type, page])
 
@@ -26,6 +32,7 @@ const MoviesList = ({type}) => {
         {type === 'top_rated' && 'Top Rated'}
         {type === 'upcoming' && 'Upcoming'}
       </h1>
+
       <div className="movies">
         {movies.map(each => (
           <MovieCard key={each.id} movie={each} />
@@ -35,13 +42,16 @@ const MoviesList = ({type}) => {
       <div className="pagination">
         <button
           type="button"
-          onClick={() => setPage(page - 1)}
+          onClick={() => setPage(prev => prev - 1)}
           disabled={page === 1}
         >
           Prev
         </button>
 
-        <button type="button" onClick={() => setPage(page + 1)}>
+        {/* THIS IS THE KEY FIX */}
+        <p>{page}</p>
+
+        <button type="button" onClick={() => setPage(prev => prev + 1)}>
           Next
         </button>
       </div>
